bbd09b648217437e298e004625f6a7f5deb06393
Sergey <wice24@gmail.com>
2015-09-30 18:05:18 +0300

    add build script

---
+++ build.js
@@ -0,0 +1,9 @@
+var fs = require("fs");
+var babel = require("babel");
+var mkdirp = require("mkdirp");
+
+mkdirp("dist", function(err) { 
+  fs.writeFileSync("./dist/Pagination.js", babel.transformFileSync("src/components/Pagination.js", {}).code);
+  fs.writeFileSync("./dist/Page.js", babel.transformFileSync("src/components/Page.js", {}).code);
+});
+
