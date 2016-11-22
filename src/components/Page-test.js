288115b2f3956ca6930f894686eed1741b6c3b9b
Raoul Jaeckel <rja@auctionata.com>
2016-11-22 21:47:23 +0100

    feat: set css class for disabled pages instead of hiding

--- src/components/Page-test.js
+++ src/components/Page-test.js
@@ -18,11 +18,12 @@ describe("<Page />", () => {
     expect(result.type).toBe("li");
   });
 
-  it("sets the active class if the page is active", () => {
+  it.only("sets the active class if the page is active", () => {
     const context = {
       ...Page.prototype,
       props: {
         pageText: "1",
+        activeClass: "active",
         pageNumber: 1,
         onClick: () => {},
         isActive: true
@@ -30,6 +31,34 @@ describe("<Page />", () => {
     };
     const result = Page.prototype.render.call(context);
     expect(result.props.className).toBe("active");
+  });
+
+  it("sets the disabled class if the page is disabled", () => {
+    const context = {
+      ...Page.prototype,
+      props: {
+        pageText: "1",
+        pageNumber: 1,
+        onClick: () => {},
+        isDisabled: true
+      }
+    };
+    const result = Page.prototype.render.call(context);
+    expect(result.props.className).toBe("disabled");
+  });
+
+  it("is not disabled by default", () => {
+    const context = {
+      ...Page.prototype,
+      props: {
+        pageText: "1",
+        pageNumber: 1,
+        onClick: () => {},
+        isDisabled: true
+      }
+    };
+    const result = Page.prototype.render.call(context);
+    expect(result.props.className).toBe("disabled");
   });
 
   it("renders an element as a child if passed an one", () => {
--- src/components/Page.js
+++ src/components/Page.js
@@ -9,22 +9,44 @@ export default class Page extends Component {
         ]),
         pageNumber: PropTypes.number.isRequired,
         onClick: PropTypes.func.isRequired,
-        isActive: PropTypes.bool.isRequired
+        isActive: PropTypes.bool.isRequired,
+        isDisabled: PropTypes.bool,
+        activeClass: PropTypes.string,
+        disabledClass: PropTypes.string
+    }
+
+    static defaultProps = {
+        activeClass: "active",
+        disabledClass: "disabled",
+        isActive: false,
+        isDisabled: false
     }
 
     render() {
-        const text = this.props.pageText || this.props.pageNumber;
-        const activeClass = this.props.activeClass || "active";
+        const {
+          pageText,
+          pageNumber,
+          activeClass,
+          disabledClass,
+          isActive,
+          isDisabled
+        } = this.props;
+
+        const text = pageText || pageNumber;
+        const css = cx({
+          [activeClass]: isActive,
+          [disabledClass]: isDisabled
+        });
 
         if (React.isValidElement(text)) {
             return text;
         }
 
         return (
-            <li className={cx({ [activeClass]: this.props.isActive })}>
+            <li className={css}>
                 <a onClick={ (e) => {
                     e.preventDefault();
-                    this.props.onClick(this.props.pageNumber);
+                    this.props.onClick(pageNumber);
                 }} href="#">
                     { text }
                 </a>
